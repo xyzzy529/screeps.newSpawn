38dd91fa64ca25b91ceeca98cd7807febbb06d0f
Bryan <xyzzy529@gmail.com>
2020-06-28 23:50:20 -0500

    Add Ruins to rechargables

--- src/main.js
+++ src/main.js
@@ -3016,7 +3016,8 @@ Object.defineProperty(Room.prototype, 'rechargeables', {
         if (!this._rechargeables) {
             this._rechargeables = [...this.storageUnits,
                 ...this.droppedEnergy,
-                ...this.tombstones];
+                ...this.tombstones,
+		...this.ruins];
         }
         return this._rechargeables;
     },
